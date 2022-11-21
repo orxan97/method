10ab22906119ed65ae97e9e35db15610937470d7
orxan97 <orkhanna@code.edu.az>
2022-11-21 11:48:34 +0400

    Add project files.

---
+++ method task/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace method_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            char opr = Convert.ToChar(Console.ReadLine ());
+            
+            Console.WriteLine(calculator(num1, num2, opr));
+
+        } 
+        static int calculator( int num1, int num2, char opr)
+        {
+            int result = 0;
+            switch (opr) 
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+            return result;
+
+
+        }
+
+    }
+}
